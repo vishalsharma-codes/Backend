dd6703360923b92a527324d8e9aa90194a3aeaef
Your Name <youremail@example.comV>
2026-07-28 00:38:23 +0530

    feat: add tweet functionality with routes, controller, and model

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
 
 app.get("/", (req, res) => {
     res.send("ROOT WORKING");
@@ -31,5 +32,6 @@ app.use("/api/v1/comments", (req, res, next) => {
 app.use("/api/v1/users" , userRouter)
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/tweets", tweetRouter)
 
 export default app;
