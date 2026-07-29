499c88f1a6ea6b92cd7413c488ec7cf4fb888f50
Your Name <youremail@example.comV>
2026-07-29 14:59:38 +0530

    feat: add like functionality with routes, controller, and model

--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ import commentRouter from "./routes/comment.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import likeRouter from "./routes/like.routes.js"
 
 app.get("/", (req, res) => {
     res.send("ROOT WORKING");
@@ -37,5 +38,6 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/likes",likeRouter);
 
 export default app;
